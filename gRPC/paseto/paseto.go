1c0ab377e306cff615e92829d9e29fdad672b74a
Khan / 이창민 <EeeasyCode@gmail.com>
2024-12-24 01:40:52 +0900

    feat: paseto 로직 간단하게 구현

--- gRPC/paseto/paseto.go
+++ gRPC/paseto/paseto.go
@@ -1,6 +1,7 @@
 package paseto
 
 import (
+	"crypto/rand"
 	"github.com/o1egl/paseto"
 	"high-traffic-practice/config"
 	auth "high-traffic-practice/gRPC/proto"
@@ -19,9 +20,13 @@ func NewPasetoMaker(cfg *config.Config) *PasetoMaker {
 }
 
 func (m *PasetoMaker) CreateNewToken(auth *auth.AuthData) (string, error) {
-	return "", nil
+	randomBytes := make([]byte, 16)
+	rand.Read(randomBytes)
+
+	return m.Pt.Encrypt(m.Key, auth, randomBytes)
 }
 
 func (m *PasetoMaker) VerifyToken(token string) error {
-	return nil
+	var authData *auth.AuthData
+	return m.Pt.Decrypt(token, m.Key, authData, nil)
 }
